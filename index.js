ae1681f8f8d77f0cce5c7538bc3b25a26472b862
Usama Abdul Moeed <usama.moeed@devsinc.com>
2024-04-04 04:39:01 +0500

    * db.js: Configured MongoDB Atlas * models.js: Added database models * schema.js: Added GraphQL schema file

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Connect to database
+connectDB();
+
 app.use(
   '/graphql',
   graphqlHTTP({
     schema,
-    graphical: process.env.NODE_ENV === 'development',
+    graphiql: process.env.NODE_ENV === 'development',
   })
 );
 
